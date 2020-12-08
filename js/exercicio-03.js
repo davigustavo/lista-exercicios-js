36e6e71437b6b98df41b0c9deab2008795589b23
Davi Gustavo <davigustavo16@outlook.com>
2020-12-08 20:09:16 -0300

    Resolução do exercicio 2

--- js/exercicio-03.js
+++ js/exercicio-03.js
@@ -1,12 +1,15 @@
-function Trocar() {
-    let numero = document.getElementById("numero").value;
-    if(numero<100) {
-        document.getElementById("mensagem").innerText = "MENOR QUE 100";
+function Soma() {
+    let numero1 = document.getElementById("numero1").value;
+    let numero2 = document.getElementById("numero2").value;
+    let resultado = parseFloat(numero1) + parseFloat(numero2);
+
+    if (resultado == 0) {
+        document.getElementById("mensagem").innerText = "Neutro";
     }
-    else if(numero == 100) {
-        document.getElementById("mensagem").innerText = "É 100";
+    else if (resultado > 0) {
+        document.getElementById("mensagem").innerText = "Positivo";
     }
-    else{
-        document.getElementById("mensagem").innerText = "MAIOR QUE 100";
-    } 
+    else {
+        document.getElementById("mensagem").innerText = "Negativo";
+    }
 }
